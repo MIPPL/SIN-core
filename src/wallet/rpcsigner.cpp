c88orey/bitcoin

[rpc] add external signer RPC files

src/wallet/rpcsigner.cpp
@@ -1,0 +1,24 @@
+// Copyright (c) 2018-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <rpc/server.h>
+#include <util/strencodings.h>
+#include <wallet/rpcsigner.h>
+#include <wallet/wallet.h>
+
+#ifdef HAVE_BOOST_PROCESS
+
+// clang-format off
+static const CRPCCommand commands[] =
+{ //  category              name                                actor (function)                argNames
+    //  --------------------- ------------------------          -----------------------         ----------
+};
+// clang-format on
+
+void RegisterSignerRPCCommands(interfaces::Chain& chain, std::vector<std::unique_ptr<interfaces::Handler>>& handlers)
+{
+    for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
+        handlers.emplace_back(chain.handleRpc(commands[vcidx]));
+}
+#endif // HAVE_BOOST_PROCESS

src/wallet/rpcsigner.h
@@ -1,0 +1,24 @@
+// Copyright (c) 2018-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_RPCSIGNER_H
+#define BITCOIN_WALLET_RPCSIGNER_H
+
+#include <util/system.h>
+#include <vector>
+
+#ifdef HAVE_BOOST_PROCESS
+
+class CRPCTable;
+
+namespace interfaces {
+class Chain;
+class Handler;
+}
+
+void RegisterSignerRPCCommands(interfaces::Chain& chain, std::vector<std::unique_ptr<interfaces::Handler>>& handlers);
+
+#endif // BOOST_VERSION
+
+#endif //BITCOIN_WALLET_RPCSIGNER_H

MIPPL/SIN-core

src/wallet/rpcsigner.cpp
@@ -1,0 +1,24 @@
+// Copyright (c) 2018-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <rpc/server.h>
+#include <util/strencodings.h>
+#include <wallet/rpcsigner.h>
+#include <wallet/wallet.h>
+
+#ifdef ENABLE_EXTERNAL_SIGNER
+
+// clang-format off
+static const CRPCCommand commands[] =
+{ //  category              name                                actor (function)                argNames
+    //  --------------------- ------------------------          -----------------------         ----------
+};
+// clang-format on
+
+void RegisterSignerRPCCommands(interfaces::Chain& chain, std::vector<std::unique_ptr<interfaces::Handler>>& handlers)
+{
+    for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
+        handlers.emplace_back(chain.handleRpc(commands[vcidx]));
+}
+#endif // ENABLE_EXTERNAL_SIGNER

src/wallet/rpcsigner.h
@@ -1,0 +1,24 @@
+// Copyright (c) 2018-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_WALLET_RPCSIGNER_H
+#define BITCOIN_WALLET_RPCSIGNER_H
+
+#include <util/system.h>
+#include <vector>
+
+#ifdef ENABLE_EXTERNAL_SIGNER
+
+class CRPCTable;
+
+namespace interfaces {
+class Chain;
+class Handler;
+}
+
+void RegisterSignerRPCCommands(interfaces::Chain& chain, std::vector<std::unique_ptr<interfaces::Handler>>& handlers);
+
+#endif // BOOST_VERSION
+
+#endif //BITCOIN_WALLET_RPCSIGNER_H